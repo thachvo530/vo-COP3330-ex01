393101f812a6cbcd72b137c2f0fdbd117d2266bb
Thach Vo <thachvo530@yahoo.com>
2021-09-11 17:16:20 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Thach Vo
+ */
+
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String nameinput = scan.next();
+
+        System.out.print("Hello, " + nameinput + ", nice to meet you");
+
+    }
+}
